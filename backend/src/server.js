a3d231a3e20e661fa53b8bdf4354dae02181564c
lucas <lcsgborges@gmail.com>
2025-04-25 22:36:54 -0300

    fix: permitir rota frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,14 @@
 import express from "express";
 import {PORT} from "./config/env.js";
 import adminRouter from "./routes/admin.routes.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    credentials: true,                
+}))
 
 app.use(express.json());
 
