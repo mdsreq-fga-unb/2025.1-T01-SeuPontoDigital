e2ec8047a23874b58b75921d6d743f75b7e13097
lucas <lcsgborges@gmail.com>
2025-05-02 00:03:59 -0300

    feat: fazer rota e logica de deletar empregado

--- backend/src/routes/private.route.js
+++ backend/src/routes/private.route.js
@@ -3,6 +3,7 @@ import getEmployees from "../controllers/Employees/getEmployees.js";
 import authVerifyToken from "../middlewares/authVerifyToken.js";
 import postEmployee from "../controllers/Employees/postEmployee.js";
 import updateEmployee from "../controllers/Employees/updateEmployee.js";
+import deleteEmployee from "../controllers/Employees/deleteEmployee.js";
 
 const privateRoute = express.Router();
 
@@ -13,6 +14,7 @@ privateRoute.use(authVerifyToken);
 privateRoute.get("/empregados", getEmployees);
 privateRoute.post("/empregados", postEmployee);
 privateRoute.put("/empregados/:id", updateEmployee);
+privateRoute.delete("/empregados/:id", deleteEmployee);
 
 // ============= EMPLOYERS =============
 
