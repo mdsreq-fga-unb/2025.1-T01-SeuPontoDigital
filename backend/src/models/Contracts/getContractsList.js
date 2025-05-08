c2ef3248e8fa786b041f3cf73e940a1d81e325c3
Guilherme Moura <107576761+Guilherme-Moura@users.noreply.github.com>
2025-05-08 18:44:50 -0300

    (feat) refatora getContractList

--- backend/src/models/Contracts/getContractsList.js
+++ backend/src/models/Contracts/getContractsList.js
@@ -2,17 +2,34 @@ import supabase from "../../config/supabase.js";
 
 const fetchContracts = async () => {
     try {
-        const { data, error } = await supabase.from("employment_contract")
-            .select(`
-            id, active, function, salary, date_start, 
-            employee:id_employee(id, name), 
-            employer:id_employer(id, name)
-          `);
-        if (error) {
-            console.error(error);
-            return null;
-        }
-        return data;
+            const { data: employmentData, error: employmentError } = await supabase.from("employment_contract")
+                .select(`
+                id, function, salary, date_start, 
+                employee:id_employee(id, name), 
+                employer:id_employer(id, name)
+              `);
+
+            const { data: contractData, error: contractError } = await supabase
+                .from("contract")
+                .select("id, id_user, active");
+            
+            if (!employmentError && !contractError) {
+                const mergedData = employmentData.map(emp => {
+                    const contractInfo = contractData.find(c => c.id_user === emp.employer.id);
+                    return {
+                        ...emp,
+                        employer: {
+                            ...emp.employer,
+                            active: contractInfo.active
+                        }
+                    };
+                });
+
+              return mergedData;
+            }
+
+            return { error: { employmentError, contractError} };
+            
     }
     catch (err) {
         console.error("error in fetchContracts models:", err);
