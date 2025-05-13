d9bad77718c2de89f4614dfa0f38751479ffd92b
Lucas Guimarães Borges <lcsgborges@gmail.com>
2025-05-13 00:11:28 -0300

    Revert "refactor: simplificar componente loginForm usando hooks e modularizar código"
    This reverts commit 16487b4fc988aaf3ae2deee1255987e7eac34437.

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -3,10 +3,7 @@ import "./Header.css"
 const Header = (props) => {
     return (
         <header className="container-main-header">
-            <div className="container-header">
-                    <img src="./images/seupontodigital.png"></img>
-                    <h1>SeuPontoDigital</h1>
-                </div> 
+            {props.children}
         </header>
     )
 }
