a01e28defc26d106b2699bdea4c6357ac534de57
Lucas Guimarães Borges <131381377+lcsgborges@users.noreply.github.com>
2025-05-15 19:39:51 -0300

    Update index.jsx

--- frontend/src/pages/AddEmployer/index.jsx
+++ frontend/src/pages/AddEmployer/index.jsx
@@ -44,7 +44,7 @@ const AddEmployer = () => {
         try {
             const token = localStorage.getItem("token");
             await axios.post(
-                `${import.meta.env.VITE_API_URL}/employer`,
+                `${import.meta.env.VITE_API_URL}/api/employer`,
                 employer,
                 {
                     headers: { Authorization: `Bearer ${token}` },
