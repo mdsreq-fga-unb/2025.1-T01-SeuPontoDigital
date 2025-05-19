cdc1e0c9d6d2f22f2cad34d4875380c858b2463b
lucas <lcsgborges@gmail.com>
2025-05-19 14:25:56 -0300

    Revert "feat(models): adiciona model para retornar dados de empregados ativos e inativos"
    This reverts commit e77b57fddc7ba469f8ecf92edd4820e38ca1cecb.

--- backend/src/controllers/Employers/getOneEmployerController.js
+++ backend/src/controllers/Employers/getOneEmployerController.js
@@ -1,16 +1,17 @@
-import getOneEmployerWithContractsModel from "../../models/Employers/getOneEmployerWithContractsModel.js";
+import getOneEmployerModel from "../../models/Employers/getOneEmployerModel.js";
 
 const getOneEmployerController = async (req, res) => {
     try {
         const employerID = req.params.id;
-        const employer = await getOneEmployerWithContractsModel(employerID);
 
-        if (!employer) {
-            return res.status(404).send({ message: "employer not found" });
+        const employer = await getOneEmployerModel(employerID);
+        if (!employer){
+            return res.status(404).send({message: "employer not found"});
         }
 
         return res.status(200).json(employer);
-    } catch (err) {
+    } 
+    catch (err) {
         return res.status(500).json({ message: "internal server error" });
     }
 };
