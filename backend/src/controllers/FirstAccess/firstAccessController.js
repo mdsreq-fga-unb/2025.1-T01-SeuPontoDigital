aa122649b4ecfc2319eb12d801479821c11afc60
lcsgborges <lcsgborges@gmail.com>
2025-06-03 22:16:28 -0300

    feat: enviar codigo de autenticacao via sms para empregador

--- backend/src/controllers/FirstAccess/firstAccessController.js
+++ backend/src/controllers/FirstAccess/firstAccessController.js
@@ -1,7 +1,6 @@
 import getOneEmployeeFromCPF from "../../models/ContractEmployee/getOneEmployeeFromCPF.js";
-// import getOneEmployerModel from "../../models/Employers/getOneEmployerModel.js";
+import getOneEmployerFromCPF from "../../models/Employers/getOneEmployerFromCPF.js";
 import sendSMS from "../../middlewares/sendSMS.js";
-
 
 const firstAccessController = async (req, res) => {
 
@@ -29,13 +28,28 @@ const firstAccessController = async (req, res) => {
     catch (err) {
 
         try {
-
+            const employer = await getOneEmployerFromCPF(cpf);
+        if (!employer) {
+            return res.status(404).send({ message: "employer not found" });
+        }
+        if (name === employer.name && phone === employer.phone) {
+            try {
+                const verification = await sendSMS(phone);
+                return res.status(200).send({
+                    message: 'send!',
+                    sid: verification.sid
+                });
+            } catch (error) {
+                return res.status(500).send({ message: error.message });
+            }
+        } else {
+            return res.status(400).send({ message: 'incorrect data' });
+        }
         }
         catch (err) {
-
+            return res.status(500).send({ message: "error in fetch data" });
         }
     }
-
 }
 
 export default firstAccessController;
