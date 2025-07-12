4e81d5f461ad54c162d939bcaec3c45daf0530df
caio-venancio <caio.venancio784@gmail.com>
2025-07-12 18:10:41 -0300

    feat: todas as opções em api.ts

--- mobile/constants/api.ts
+++ mobile/constants/api.ts
@@ -9,10 +9,17 @@ import axios from 'axios';
 //   },
 // });
 
+// const api = axios.create({
+//   baseURL: 'http://192.168.0.4:3333/api',
+//   timeout: 15000,
+//   headers: { 'Content-Type': 'application/json' },
+// });
+
 const api = axios.create({
-  baseURL: 'http://192.168.0.4:3333/api',
+  baseURL: 'http://localhost:3333/api',
   timeout: 15000,
   headers: { 'Content-Type': 'application/json' },
 });
+
 
 export default api; 
