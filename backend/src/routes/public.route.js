8594a798125f1f69e53b7870a624dae9f067acf6
lcsgborges <lcsgborges@gmail.com>
2025-07-14 16:37:01 -0300

    cleanup: arquivo de rotas

--- backend/src/routes/public.route.js
+++ backend/src/routes/public.route.js
@@ -13,8 +13,8 @@ const publicRoute = express.Router();
 // ================= LOGIN WEB =================
 publicRoute.post("/login-admin", loginAdmin);
 
+
 // ================= LOGIN APP =================
-// publicRoute.post("/login-employer", loginEmployer);
 publicRoute.post("/login", loginAdmin);
 publicRoute.post("/login-app", loginApp);
 
@@ -23,8 +23,10 @@ publicRoute.post("/login-app", loginApp);
 publicRoute.post("/first-access", validatePassword, firstAccessController);
 publicRoute.patch("/create-password", createPassword);
 
+
 // ======== FORGOTTEN PASSWORD ========
 publicRoute.post("/forgotten-password", forgottenPasswordController);
 publicRoute.patch("/update-password", updatePassword);
+
 
 export default publicRoute;
