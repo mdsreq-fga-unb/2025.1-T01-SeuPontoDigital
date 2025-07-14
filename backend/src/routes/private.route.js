616653489fffc85b1abe70febd169aa5eee0a2f6
davi-aguiar-vieira <davideaguiarvieira@gmail.com>
2025-07-14 18:55:59 -0300

    fix: corrige rotas

--- backend/src/routes/private.route.js
+++ backend/src/routes/private.route.js
@@ -1,107 +1,117 @@
 import express from "express";
-import validateTokenJWT from "../middlewares/validateTokenJWT.js";
-import validateUser from "../middlewares/validateUser.js";
-import validateEmployer from "../middlewares/validateEmployer.js";
-import {validateDateTwoYearsEmployee, validateDateTwoYearsEmployer, validateDateTwoYearsContract} from "../middlewares/validateDateTwoYears.js";
+import authVerifyToken from "../middlewares/authVerifyToken.js";
+
+// ========== IMPORTS DOS CONTROLLERS ==========
+
+// Address Controllers
 import postAddressController from "../controllers/Address/postAddressController.js";
 import putAddressController from "../controllers/Address/putAddressController.js";
-import getEmployersController from "../controllers/Employers/getEmployersController.js";
-import getOneEmployerController from "../controllers/Employers/getOneEmployerController.js";
-import postEmployerController from "../controllers/Employers/postEmployerController.js";
-import deleteEmployerController from "../controllers/Employers/deleteEmployerController.js";
-import putEmployerController from "../controllers/Employers/putEmployerController.js";
-import postWorklogController from "../controllers/Worklog/postWorklogController.js";
-import putWorklogController from "../controllers/Worklog/putWorklogController.js";
-import getEmployeeAndContractsController from "../controllers/Contracts/getEmployeeAndContractsController.js";
-import getTodayRecordsController from "../controllers/Worklog/getTodayRecordsController.js";
-import getRecordsController from "../controllers/Worklog/getRecordsController.js"
-import getEmployeesController from "../controllers/Employees/getEmployeesController.js";
-import getOneEmployeeController from "../controllers/Employees/getOneEmployeeController.js";
-import postEmployeeController from "../controllers/Employees/postEmployeeController.js";
-import deleteEmployeeController from "../controllers/Employees/deleteEmployeeController.js";
-import putEmployeeController from "../controllers/Employees/putEmployeeController.js";
+
+// Contract Controllers
 import getContractsController from "../controllers/Contracts/getContractsController.js";
 import getOneContractController from "../controllers/Contracts/getOneContractController.js";
 import getFullContractDataController from "../controllers/Contracts/getFullContractDataController.js";
+import getEmployeeAndContractsController from "../controllers/Contracts/getEmployeeAndContractsController.js";
 import postContractController from "../controllers/Contracts/postContractController.js";
 import postCompleteContractController from "../controllers/Contracts/postCompleteContractController.js";
 import putContractController from "../controllers/Contracts/putContractController.js";
 import deleteContractController from "../controllers/Contracts/deleteContractController.js";
-import postWorkAddressController from "../controllers/WorkAddress/postWorkAddressController.js";
-import getAllWorkAddressController from "../controllers/WorkAddress/getAllWorkAddressController.js";
-import getOneWorkAddressController from "../controllers/WorkAddress/getOneWorkAddressController.js";
-import getAllEmployController from "../controllers/Employ/getEmployController.js";
-import postEmployController from "../controllers/Employ/postEmployController.js";
+
+// Employee Controllers
+import getEmployeesController from "../controllers/Employees/getEmployeesController.js";
+import getOneEmployeeController from "../controllers/Employees/getOneEmployeeController.js";
+import postEmployeeController from "../controllers/Employees/postEmployeeController.js";
+import putEmployeeController from "../controllers/Employees/putEmployeeController.js";
+import deleteEmployeeController from "../controllers/Employees/deleteEmployeeController.js";
+
+// Employer Controllers
+import getEmployersController from "../controllers/Employers/getEmployersController.js";
+import getOneEmployerController from "../controllers/Employers/getOneEmployerController.js";
+import postEmployerController from "../controllers/Employers/postEmployerController.js";
+import putEmployerController from "../controllers/Employers/putEmployerController.js";
+import deleteEmployerController from "../controllers/Employers/deleteEmployerController.js";
+
+// SignContract Controllers
 import getAllSignContractController from "../controllers/SignContract/getAllSignContractController.js";
 import getOneSignContractController from "../controllers/SignContract/getOneSignContractController.js";
 import postSignContractController from "../controllers/SignContract/postSignContractController.js";
-import getEmployerRecordsController from "../controllers/Worklog/getEmployerRecordsController.js";
-import authVerifyToken from "../middlewares/authVerifyToken.js";
-import getAlertsController from "../controllers/Alerts/getAlertsController.js";
+
+// Employ Controllers
+import postEmployController from "../controllers/Employ/postEmployController.js";
+
+// WorkAddress Controllers
+import postWorkAddressController from "../controllers/WorkAddress/postWorkAddressController.js";
+
+// WorkSchedule Controllers
+import getAllWorkScheduleController from "../controllers/WorkSchedule/getAllWorkScheduleController.js";
+import getOneWorkScheduleController from "../controllers/WorkSchedule/getOneWorkScheduleController.js";
+import postWorkScheduleController from "../controllers/WorkSchedule/postWorkScheduleController.js";
+import putOneWorkScheduleController from "../controllers/WorkSchedule/putOneWorkScheduleController.js";
+import deleteOneWorkScheduleController from "../controllers/WorkSchedule/deleteOneWorkScheduleController.js";
+
+// WorkBreaks Controllers
+import putBreakController from "../controllers/WorkBreaks/putBreakController.js";
+
+// Worklog Controllers
+import postWorklogController from "../controllers/Worklog/postWorklogController.js";
+import putWorklogController from "../controllers/Worklog/putWorklogController.js";
 
 const privateRoute = express.Router();
 
-privateRoute.use(validateTokenJWT);
+// Aplicar middleware de autenticação em todas as rotas privadas
+privateRoute.use(authVerifyToken);
 
-// ================= ADDRESS =================
+// ========== ADDRESS ROUTES ==========
 privateRoute.post("/address", postAddressController);
 privateRoute.put("/address/:id", putAddressController);
 
-
-// ================= WORK ADDRESS =================
-privateRoute.get("/work-addresses", getAllWorkAddressController);
-privateRoute.get("/work-address", getOneWorkAddressController);
-privateRoute.post("/work-address", postWorkAddressController);
-
-
-// ================= CONTRACTS =================
+// ========== CONTRACT ROUTES ==========
 privateRoute.get("/contracts", getContractsController);
 privateRoute.get("/contract/:id", getOneContractController);
 privateRoute.get("/contract/:id/full", getFullContractDataController);
+privateRoute.get("/employee-contracts/:id", getEmployeeAndContractsController);
 privateRoute.post("/contract", postContractController);
 privateRoute.post("/contract/complete", postCompleteContractController);
 privateRoute.put("/contract/:id", putContractController);
-privateRoute.delete("/contract/:id", validateDateTwoYearsContract, deleteContractController);
+privateRoute.delete("/contract/:id", deleteContractController);
 
-
-// ================== EMPLOYERS ==================
-privateRoute.get("/employers", getEmployersController);
-privateRoute.get("/employer/:id", getOneEmployerController);
-privateRoute.post("/employer", validateEmployer, postEmployerController);
-privateRoute.put("/employer/:id", validateEmployer, putEmployerController);
-privateRoute.delete("/employer/:id", validateDateTwoYearsEmployer, deleteEmployerController);
-
-
-// ================== EMPLOYEES ==================
+// ========== EMPLOYEE ROUTES ==========
 privateRoute.get("/employees", getEmployeesController);
 privateRoute.get("/employee/:id", getOneEmployeeController);
-privateRoute.post("/employee", validateUser, postEmployeeController);
-privateRoute.put("/employee/:id", validateUser, putEmployeeController);
-privateRoute.delete("/employee/:id", validateDateTwoYearsEmployee, deleteEmployeeController);
-privateRoute.get("/employee-contracts", getEmployeeAndContractsController);
+privateRoute.post("/employee", postEmployeeController);
+privateRoute.put("/employee/:id", putEmployeeController);
+privateRoute.delete("/employee/:id", deleteEmployeeController);
 
+// ========== EMPLOYER ROUTES ==========
+privateRoute.get("/employers", getEmployersController);
+privateRoute.get("/employer/:id", getOneEmployerController);
+privateRoute.post("/employer", postEmployerController);
+privateRoute.put("/employer/:id", putEmployerController);
+privateRoute.delete("/employer/:id", deleteEmployerController);
 
-// ================= EMPLOY =================
-privateRoute.get("/employ", getAllEmployController);
-privateRoute.post("/employ", postEmployController);
-
-
-// ================= SIGN CONTRACT =================
+// ========== SIGN CONTRACT ROUTES ==========
 privateRoute.get("/sign-contract", getAllSignContractController);
 privateRoute.get("/sign-contract/:id", getOneSignContractController);
 privateRoute.post("/sign-contract", postSignContractController);
 
+// ========== EMPLOY ROUTES ==========
+privateRoute.post("/employ", postEmployController);
 
-// ================== WORKLOGS ==================
-privateRoute.get("/worklogToday/:id", getTodayRecordsController);
-privateRoute.get("/worklog", authVerifyToken, getRecordsController);
-privateRoute.get("/worklogEmployer", authVerifyToken, getEmployerRecordsController);
+// ========== WORK ADDRESS ROUTES ==========
+privateRoute.post("/work-address", postWorkAddressController);
+
+// ========== WORK SCHEDULE ROUTES ==========
+privateRoute.get("/workschedule", getAllWorkScheduleController);
+privateRoute.get("/workschedule/:id", getOneWorkScheduleController);
+privateRoute.post("/workschedule/:id", postWorkScheduleController);
+privateRoute.put("/workschedule/:id", putOneWorkScheduleController);
+privateRoute.delete("/workschedule/:id", deleteOneWorkScheduleController);
+
+// ========== WORK BREAKS ROUTES ==========
+privateRoute.put("/workbreak/:id", putBreakController);
+
+// ========== WORKLOG ROUTES ==========
 privateRoute.post("/worklog", postWorklogController);
 privateRoute.put("/worklog", putWorklogController);
-
-
-// ================== ALERTS ==================
-privateRoute.get("/alerts", authVerifyToken, getAlertsController);
-
 
 export default privateRoute;
